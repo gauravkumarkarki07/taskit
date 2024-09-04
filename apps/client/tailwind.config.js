9369fea8735f7f1bea18779a07fe2775c9c3c6de
Gaurav <gauravkumarkarki07@gmail.com>
2024-09-05 02:28:07 +1000

    Feat: Shadcn Added

--- apps/client/tailwind.config.js
+++ apps/client/tailwind.config.js
@@ -1,22 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors:{
-        primary:'#7209b7',
-        secondary:'#3a0ca3',
-        tertiary:'#4361ee',
-        accentPrimary:'#f72585',
-        accentSecondary:'#4cc9f0',
-      },
-      fontFamily:{
-        Poppins:['Poppins','sans-serif']
-      }
-    },
+  	extend: {
+  		colors: {
+  			primary: '#7209b7',
+  			secondary: '#3a0ca3',
+  			tertiary: '#4361ee',
+  			accentPrimary: '#f72585',
+  			accentSecondary: '#4cc9f0'
+  		},
+  		fontFamily: {
+  			Poppins: ['Poppins','sans-serif']
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 }
