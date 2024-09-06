74316d2196c569d0fa15311922fd21bcbbc65a3f
Gaurav <gauravkumarkarki07@gmail.com>
2024-09-07 02:26:08 +1000

    Feat: Spinner has been impletemented

--- apps/client/src/Auth/components/ProtectedRoute.tsx
+++ apps/client/src/Auth/components/ProtectedRoute.tsx
@@ -1,24 +1,24 @@
 import Sidebar from "@/Common/pages/Sidebar"
 import { Navigate, Outlet } from "react-router-dom"
 import { useVerifyToken } from "../hooks/useAuthQuery"
+import Spinner from '@/Common/Utils/Spinner'
 
 function ProtectedRoute() {
-    const{data:validUserDetails,isLoading}=useVerifyToken();
+    const { data: validUserDetails, isLoading } = useVerifyToken();
 
-    if(isLoading){
-       return( <div>
-            Loading ...
-        </div>
-       )
+    if (isLoading) {
+        return (
+            <Spinner/>
+        )
     }
 
     return (
         <section className="flex">
             <section className="w-[23%] min-h-screen">
-                <Sidebar/>
+                <Sidebar />
             </section>
             <section className="w-full px-4 py-4 bg-tertiary/5 min-h-screen">
-                {validUserDetails ? <Outlet/> : <Navigate to={'/auth/login'} replace/>}
+                {validUserDetails ? <Outlet /> : <Navigate to={'/auth/login'} replace />}
             </section>
         </section>
     )
---
+++ apps/client/src/Common/Utils/Spinner.tsx
@@ -0,0 +1,16 @@
+import ClipLoader from "react-spinners/ClipLoader";
+
+function Spinner() {
+    return (
+        <section className="flex items-center justify-center min-h-screen">
+            <ClipLoader
+                color={'#f72585'}
+                loading={true}
+                size={50}
+                aria-label="Loading Spinner"
+                data-testid="loader"
+            />
+        </section>
+    )
+}
+export default Spinner
