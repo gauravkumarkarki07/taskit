124783d219f6b3f073572680403dae3c8489e818
Gaurav <gauravkumarkarki07@gmail.com>
2024-09-06 23:39:34 +1000

    Fix: AuthModule imported on AppModule

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProjectsModule } from './Modules/Projects/projects.module';
 import { DatabaseModule } from './Modules/Database/database.module';
+import { AuthModule } from './Modules/Auth/auth.module';
 
 @Module({
-  imports: [ProjectsModule, DatabaseModule, ProjectsModule],
+  imports: [ProjectsModule, DatabaseModule, ProjectsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
